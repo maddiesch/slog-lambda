da0d0f4fbc1dc4faf2ae861a489c2b95fff874b9
Maddie Schipper <me@maddiesch.com>
2024-08-27 21:05:39 -0600

    undo

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/maddiesch/slog-lambda
 
 go 1.23.0
 
-require github.com/aws/aws-lambda-go v1.47.0
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	go.uber.org/goleak v1.3.0
+)
--- main_test.go
+++ main_test.go
@@ -3,12 +3,13 @@ package sloglambda_test
 import (
 	"os"
 	"testing"
+
+	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
 	os.Setenv("AWS_LAMBDA_FUNCTION_NAME", "test-function")
 	os.Setenv("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST")
 
-	exitCode := m.Run()
-	os.Exit(exitCode)
+	goleak.VerifyTestMain(m)
 }
