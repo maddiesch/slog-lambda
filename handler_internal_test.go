8efc510bd8f0bb8e1430945e83d75d132c4ecd48
Maddie Schipper <me@maddiesch.com>
2024-08-27 22:05:20 -0600

    add logRecord clean tests

--- handler_internal_test.go
+++ handler_internal_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_loggerLevelFromString(t *testing.T) {
@@ -49,4 +50,29 @@ func Test_lambdaLoggerLevelString(t *testing.T) {
 			assert.Equal(t, str, lambdaLoggerLevelString(level))
 		})
 	}
+}
+
+func Test_logRecord_clean(t *testing.T) {
+	t.Run("when the log record has an empty sub-record", func(t *testing.T) {
+		r := logRecord{
+			"foo": logRecord{},
+		}
+		r.clean()
+
+		_, ok := r["foo"]
+		assert.False(t, ok, "the sub-record should have been removed")
+	})
+
+	t.Run("when the log record has a non-empty sub-record", func(t *testing.T) {
+		r := logRecord{
+			"foo": logRecord{"bar": "baz", "qux": logRecord{}},
+		}
+		r.clean()
+
+		foo, ok := r["foo"]
+		require.True(t, ok, "the sub-record should not have been removed")
+
+		_, ok = foo.(logRecord)["qux"]
+		assert.False(t, ok, "the sub-record should have been removed")
+	})
 }
