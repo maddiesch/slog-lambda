7c596c0ddd88871f1ed1c35eb333d2a1aaf19ab8
Maddie Schipper <me@maddiesch.com>
2024-08-27 21:04:44 -0600

    fix windows tests

--- main_test.go
+++ main_test.go
@@ -3,13 +3,12 @@ package sloglambda_test
 import (
 	"os"
 	"testing"
-
-	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
 	os.Setenv("AWS_LAMBDA_FUNCTION_NAME", "test-function")
 	os.Setenv("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST")
 
-	goleak.VerifyTestMain(m)
+	exitCode := m.Run()
+	os.Exit(exitCode)
 }
